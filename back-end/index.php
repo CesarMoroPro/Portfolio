e074fb985ba17de298e94463f6903cc5f6bbb801
CesarMoro <cesar-moro@outlook.fr>
2021-12-29 01:07:33 +0100

    Dynamisation PHP des project-cards in progress. Titre, image, date, url fonctionnent. En revanche, seul la première project-card répond au JS, et elle affiche le 'resume' d'une autre card

--- back-end/index.php
+++ back-end/index.php
@@ -1,30 +1,33 @@
 <?php
 
 $projects = [
-                ["portfolio"    =>      ["titre"    => "Portfolio",
-                                        "excerp"    => "Comme pour tout créateur, chaque projet que je réalise alimente mon portfolio. Vous vous trouvez justement sur cette réalisation."
-                                        ],
-                "voyages"       =>      ["titre"    => "Voyages",
-                                        "exercp"    => "Des aurores boérales de Norvège aux forêts tropicales des îles d'Hawaii, parcourez ce projet pour découvrir une partie du monde."
-                                        ],
-                "cinéma"        =>      ["titre"    => "Cinéma",
-                                        "excerp"    => "Avec DeadPool, ça débite et ça déboite ! Wham !"
-                                        ],
-                "sports"        =>      ["titre"    => "Sports",
-                                        "excerp"    => "Plongez dans le Pacifique avec les requins, ou prenez de la hauteur en wingsuit au-dessus du monde"
-                                        ]
+                [
+                    "titre"     => "Portfolio",
+                    "resume"    => "Comme pour tout créateur, chaque projet que je réalise alimente mon portfolio. Vous vous trouvez justement sur cette réalisation.",
+                    "image"     => "../front-end/assets/img/portfolio-card.jpg",
+                    "url"       => "http://google.com",
+                    "date"      => "28 12 2021"
+                ],
+                [
+                    "titre"     => "Voyages",
+                    "resume"    => "Des aurores boérales de Norvège aux forêts tropicales des îles d'Hawaii, parcourez ce projet pour découvrir une partie du monde.",
+                    "image"     => "../front-end/assets/img/voyages-card.jpeg",
+                    "url"       => "http://oclock.io",
+                    "date"      => "05 09 2021"
+                ],
+                [
+                    "titre"     => "Cinéma",
+                    "resume"    => "Avec DeadPool, ça débite et ça déboite ! Wham !",
+                    "image"     => "../front-end/assets/img/test6-card.jpg",
+                    "url"       => "https://www.ironman.com",
+                    "date"      => "01 02 2021"
                 ]
 ];
-
-var_dump($projects . "<br>");
-echo "<br>";
-// $projectsLength = count(projects);
-// for (i = 0; i <= projects-length; i++){
-//     $project = project[i];
-// }
-// var_dump(project);
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -33,9 +36,9 @@ echo "<br>";
     <!-- link main css-->
     <link rel="stylesheet" href="../front-end/css/main.css"/>
     
-    <!-- link swiper css-->
+    <!-- link CDN swiper css-->
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
-    
+
     <!-- link fontawesome css-->
     <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.css">
 
@@ -71,8 +74,7 @@ echo "<br>";
     <main class="home__content">
         <section id="home__content__bio">
             <h3 class="home__content__title__h3"><i class="icons far fa-user"></i>Mini-bio</h3>
-            <p class="home__content__text-normal">Bonjour à tous, et merci de vous intéresser à moi.<br>
-            Une petite présentation personnelle s'impose, avant de découvrir les projets de développement web sur lesquels j'ai travaillé.</p>
+            <p class="home__content__text-normal">Bonjour à tous, et merci de vous intéresser à moi.<br> Une petite présentation personnelle s'impose, avant de découvrir les projets de développement web sur lesquels j'ai travaillé.</p>
             <p class="home__content__text-normal">Avant de m'orienter dans le développement web et web mobile, j'étais Responsble d'inventaires Europe pour une société spécialisée dans ce domaine. J'y ai offert mes services, en France comme à l'étranger, du Portugal à la Roumanie en passant par la Norvège ; Et dans différents secteurs d'activités, du petit magasin Retail au gros entrepôt de valeur marchande quelques dizaines de millions d'euros.<br>
             J'y ai développé de la rigueur, de la relation clients, et une totale autonomie. De l'autonomie, oui ! Toutefois, j'étais régulièrement amené à travailler en équipe avec mes collègues, et c'est avec plaisir que nous collaborions tous ensemble, à l'écoute des idées de chacun.</p>
             <p class="home__content__text-normal">Est ensuite venu pour moi le temps de la reconversion. Par manque de temps dans ma vie personnelle, mais également par de nouvelles envies de vivre.<br>
@@ -81,33 +83,39 @@ echo "<br>";
             Voilà pourquoi vous pouvez me faire confiance pour la création et le développement de votre projet web.</p>
         </section>
 
-        <h3 class="home__content__title__h3"><i class="icons fas fa-laptop-code"></i>Projets de Web'Developement</h3>
+        <h3 class="home__content__title__h3"><i class="icons fas fa-laptop-code"></i>Réalisations de Développement Web</h3>
+        <!--   
+        <div class="global-projects">
+            <div class="grid ">
+                <button class="grid-item javascript"    data-filter="test-1">JavaScript</button>
+                <button class="grid-item jquery"        data-filter="test-2">jQuery</button>
+                <button class="grid-item swiperjs"      data-filter="test-3">Swiper</button>
+                <button class="grid-item isotope"       data-filter="test-4">Isotope</button>
+                <button class="grid-item sass"          data-filter="test-5">Sass</button>
+            </div>
+        -->
 
-        <section class="home__content__projects">
-            
-            
+            <section class="home__content__projects">
+            <?php foreach($projects as $project): ;?>
                 <div class="home__content__project-card">
-                    <div class="home__content__project-card__divs">
+                    <a class="home__content__project-card__divs__div-link" href="<?= $project['url']; ?>"><div class="home__content__project-card__divs">
                         <div class="home__content__project-card__divs__div-text">
-                            <p class="home__content__project-card__text"><?= project[1]; ?></p>
-                            <p class="home__content__project-card__text-date">Date de dernière mise à jour : <span class="date">25 mars 2021</span></p>
+                            <p class="home__content__project-card__text transition__text__disappears"><?= $project['resume'];?></p>
+                            <p class="home__content__project-card__text-date transition__date__disappears">Date de dernière mise à jour : <span class="date"><?= $project['date']; ?></span></p>
                         </div>
 
                         <div class="home__content__project-card__divs__div-img">
-                            <h2 class="home__content__project-card__title"><i class="fa-solid fa-map-location-dot"></i>
-                                <a class="link link-title" href="http://google.com" alt="lien vers le projet Porfolio">
-                                    <?= project[0]; ?>
-                                </a>
+                            <h2 class="home__content__project-card__title transition__title__appears">
+                                <i class="icons fa-solid fa-map-location-dot"></i>
+                                    <?= $project['titre'];?>
                             </h2>
-                            <a class="home__content__project-card__img-link" href="http://google.com" alt="lien vers le projet Portfolio">
-                                <img class="home__content__project-card__img" src="../front-end/assets/img/portfolio-card.jpg">
-                            </a>
+                            <img class="home__content__project-card__img unblurry" src="<?= $project['image'];?>" >
                         </div>    
-                    </div>
+                    </div></a>
                 </div>
-
-               
-        </section>
+            <?php endforeach; ?>
+            </section>
+        </div>
     </main>
 
     <footer class="home__footer">
--- front-end/js/components/projectCard.js
+++ front-end/js/components/projectCard.js
@@ -3,68 +3,71 @@ let projects =  {
                                 'title'     : 'Portfolio',
                                 'excerp'    : 'Comme pour tout créateur, chaque projet que je réalise alimente mon portfolio. Vous vous trouvez justement sur cette réalisation.',
                                 'date'      : '',
-                                'url'       : ''
+                                'url'       : '',
+                                'image'     : 'src="./front-end/assets/img/portfolio-card.jpg"'
                 },
                 'voyages'   :   {
                                 'title'     : 'Voyages',
                                 'excerp'    : 'Des aurores boérales de Norvège aux forêts tropicales des îles d\'Hawaii, parcourez ce projet pour découvrir une partie du monde.',
                                 'date'      : '',
-                                'url'       : ''
+                                'url'       : '',
+                                'image'     : 'src="./front-end/assets/img/voyages-card.jpg"'
                 },
                 'cinema'    :   {
                                 'title'     : 'Cinéma',
                                 'excerp'    : 'Avec DeadPool, ça débite et ça déboite ! Wham !',
                                 'date'      : '',
-                                'url'       : ''
+                                'url'       : '',
+                                'image'     : 'src="./front-end/assets/img/test6-card"'
                 },
+                /*
                 'sport'     :   {
                                 'title'     : 'Sport',
                                 'excerp'    : 'Plongez dans le Pacifique avec les requins, ou prenez de la hauteur en wingsuit au-dessus du monde',
                                 'date'      : '',
                                 'url'       : ''
                 }
+                */
 }
 
 for (let project in projects){
-    console.log(projects[project]['title']);
-    console.log((projects[project]['excerp']));
+    for (let index in project){
+        //^ Creation of the variables to be handled
+
+        let cardProject     = document.querySelector('.home__content__project-card');
+        let cardImage       = document.querySelector('.home__content__project-card__img');
+        let cardTitle       = document.querySelector('.home__content__project-card__title');
+        let cardText        = document.querySelector('.home__content__project-card__text')
+        let cardDate        = document.querySelector('.home__content__project-card__text-date');
+
+
+        //^ Creation of functions
+
+        function blurryCard() {
+
+            cardImage.innerHTML = '<img class="home__content__project-card__img unblurry"' + projects[project][index] + '>';
+            cardImage.classList.replace('unblurry', 'blurry');
+            cardTitle.classList.replace('transition__title__appears', 'transition__title__disappears'); 
+            cardText.classList.replace('transition__text__disappears', 'transition__text__appears');
+            cardDate.classList.replace('transition__date__disappears', 'transition__date__appears');
+
+            //cardText.textContent = projects[project]['excerp'];
+        };
+
+        function unblurryCard() {
+
+            cardImage.classList.replace('blurry', 'unblurry');
+            cardTitle.classList.replace('transition__title__disappears', 'transition__title__appears');
+            cardText.classList.replace('transition__text__appears', 'transition__text__disappears');
+            cardDate.classList.replace('transition__date__appears', 'transition__date__disappears');
+        };
+
+
+        //^ Creation of EventListeners
+        cardProject.addEventListener('mouseover', blurryCard);
+
+
+        //^ Suppression of EventListeners
+        cardProject.addEventListener('mouseout', unblurryCard);
+    }
 }
-
-//^ Creation of the variables to be handled
-
-let cardProject     = document.querySelector('.home__content__project-card');
-let cardImage       = document.querySelector('.home__content__project-card__img');
-let cardTitle       = document.querySelector('.home__content__project-card__title');
-let cardText        = document.querySelector('.home__content__project-card__text')
-let cardDate        = document.querySelector('.home__content__project-card__text-date');
-
-let excerp = document.createElement('div');
-
-
-//^ Creation of functions
-
-function blurryCard() {
-
-    cardImage.classList.replace('unblurry', 'blurry');
-    cardTitle.classList.replace('transition__title__appears', 'transition__title__disappears'); 
-    cardText.classList.replace('transition__text__disappears', 'transition__text__appears');
-    cardDate.classList.replace('transition__date__disappears', 'transition__date__appears');
-
-    cardText.textContent = projects['portfolio']['excerp'];
-};
-
-function unblurryCard() {
-
-    cardImage.classList.replace('blurry', 'unblurry');
-    cardTitle.classList.replace('transition__title__disappears', 'transition__title__appears');
-    cardText.classList.replace('transition__text__appears', 'transition__text__disappears');
-    cardDate.classList.replace('transition__date__appears', 'transition__date__disappears');
-};
-
-
-//^ Creation of EventListeners
-cardProject.addEventListener('mouseover', blurryCard);
-
-
-//^ Suppression of EventListeners
-cardProject.addEventListener('mouseout', unblurryCard);
