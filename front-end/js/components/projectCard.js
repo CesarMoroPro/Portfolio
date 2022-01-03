1c7d6a4a0ef9cee216b3a75425a241098d3a2a5c
CesarMoro <cesar-moro@outlook.fr>
2022-01-03 16:33:07 +0100

    Dynamisation des cartes terminées. Toutes les infos sont récupérées via le tableau en JS. Ne reste plus qu'à boucler pour afficher toutes les cartes

--- front-end/js/components/projectCard.js
+++ front-end/js/components/projectCard.js
@@ -1,57 +1,57 @@
+let imgSrc = "./front-end/assets/img/";
 let projects =  {
                 'portfolio' :   {
                                 'title'     : 'Portfolio',
                                 'excerp'    : 'Comme pour tout créateur, chaque projet que je réalise alimente mon portfolio. Vous vous trouvez justement sur cette réalisation.',
-                                'date'      : '',
-                                'url'       : '',
-                                'image'     : 'src="./front-end/assets/img/portfolio-card.jpg"'
+                                'date'      : '29 12 2021',
+                                'url'       : 'http://google.com',
+                                'image'     : imgSrc + 'portfolio-card.jpg'
                 },
                 'voyages'   :   {
                                 'title'     : 'Voyages',
                                 'excerp'    : 'Des aurores boérales de Norvège aux forêts tropicales des îles d\'Hawaii, parcourez ce projet pour découvrir une partie du monde.',
-                                'date'      : '',
-                                'url'       : '',
-                                'image'     : 'src="./front-end/assets/img/voyages-card.jpg"'
+                                'date'      : '30 04 2018',
+                                'url'       : 'https://www.gohawaii.com/fr',
+                                'image'     : imgSrc + 'voyages-card.jpeg'
                 },
                 'cinema'    :   {
                                 'title'     : 'Cinéma',
                                 'excerp'    : 'Avec DeadPool, ça débite et ça déboite ! Wham !',
-                                'date'      : '',
-                                'url'       : '',
-                                'image'     : 'src="./front-end/assets/img/test6-card"'
-                },
-                /*
-                'sport'     :   {
-                                'title'     : 'Sport',
-                                'excerp'    : 'Plongez dans le Pacifique avec les requins, ou prenez de la hauteur en wingsuit au-dessus du monde',
-                                'date'      : '',
-                                'url'       : ''
+                                'date'      : '03 11 2016',
+                                'url'       : 'https://www.allocine.fr/film/fichefilm_gen_cfilm=146349.html',
+                                'image'     : imgSrc + 'test6-card.jpg'
                 }
-                */
 }
 
 for (let project in projects){
-    for (let index in project){
-        //^ Creation of the variables to be handled
+    //^ Creation of the variables to be handled
 
-        let cardProject     = document.querySelector('.home__content__project-card');
-        let cardImage       = document.querySelector('.home__content__project-card__img');
-        let cardTitle       = document.querySelector('.home__content__project-card__title');
-        let cardText        = document.querySelector('.home__content__project-card__text')
-        let cardDate        = document.querySelector('.home__content__project-card__text-date');
+    let cardProject     = document.querySelector('.home__content__project-card');
+    let cardImage       = document.querySelector('.home__content__project-card__img');
+    let cardTitle       = document.querySelector('.home__content__project-card__title');
+    let cardText        = document.querySelector('.home__content__project-card__text')
+    let cardDate        = document.querySelector('.home__content__project-card__text-date');
+    let cardUrl         = document.querySelector('.home__content__project-card__divs__div-link');
+  
+
+    for (let index in project){
+
+        cardImage.setAttribute('src', projects[project]['image']);
+        cardUrl.setAttribute('href', projects[project]['url']);
+        
+        cardDate.innerHTML      = "Date de dernière mise à jour : <span class='date'></span>" + projects[project]['date'] + "</span></p>";
+
+        cardTitle.textContent   = projects[project]['title'];
+        cardText.textContent    = projects[project]['excerp'];
 
 
         //^ Creation of functions
-
         function blurryCard() {
 
-            cardImage.innerHTML = '<img class="home__content__project-card__img unblurry"' + projects[project][index] + '>';
             cardImage.classList.replace('unblurry', 'blurry');
             cardTitle.classList.replace('transition__title__appears', 'transition__title__disappears'); 
             cardText.classList.replace('transition__text__disappears', 'transition__text__appears');
             cardDate.classList.replace('transition__date__disappears', 'transition__date__appears');
-
-            //cardText.textContent = projects[project]['excerp'];
         };
 
         function unblurryCard() {
@@ -62,7 +62,7 @@ for (let project in projects){
             cardDate.classList.replace('transition__date__appears', 'transition__date__disappears');
         };
 
-
+    
         //^ Creation of EventListeners
         cardProject.addEventListener('mouseover', blurryCard);
 
