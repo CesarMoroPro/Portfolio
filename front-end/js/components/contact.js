7a1f36c0254bc258f2dbc1ad0ae665342a76298d
CesarMoro <cesar-moro@outlook.fr>
2022-02-03 11:50:21 +0100

    Formulaire de contact : Zoom au survol de la croix + condition pour ne pas ouvrir plusieurs formulaires en meme temps

--- front-end/js/components/contact.js
+++ front-end/js/components/contact.js
@@ -1,49 +1,63 @@
-let global = document.getElementById('global');
-let contact = document.querySelectorAll('.contactForm');
-let screen = document.getElementById('screen');
+let global      = document.getElementById('global');
+let contact     = document.querySelectorAll('.contactForm');
+let screen      = document.getElementById('screen');
+let divForm     = document.createElement('div');
+let cross       = document.createElement('div');
+let closedForm  = true;
+
 
 contact.forEach(element => {
-    element.addEventListener('click', formScreen);
-        
-    //^ Creation of function
-    function formScreen() {
-        let elements  = document.querySelectorAll('.hide-for-form');
-
-        //^ Functions to get and manipulate elements
-        (function hideElements() {
-            
-            elements.forEach(element =>
-                element.classList.replace('notHiding', 'hiding'));
-        })();
-
-        function quitTheForm() {
-            divForm.remove();
-
-            elements.forEach(element =>
-                element.classList.replace('hiding', 'notHiding'));
-
-            screen.classList.remove('blurryScreen');
-
-        };
-
-        
-        //^ ClassList
-        screen.classList.add('blurryScreen');
-
-        //^ Creation of new elements
-        //? New div for the form
-        let divForm = document.createElement('div');
-        global.append(divForm);
-        divForm.classList.add('contact-form');
-        divForm.innerHTML = "<h3 class='home__content__title__h3 title-form'>Formulaire de contact</h3>";
-        
-
-        //? New div for the cross to quit
-        let cross = document.createElement('div');
-        divForm.append(cross);
-        cross.innerHTML = "<i class=\"far fa-times-circle cross-to-quit\"></i>";
-
-        cross.addEventListener('click', quitTheForm);
-        
-    };
+    element.addEventListener('click', openForm)
 });
+
+
+
+//^ Creation of function to open the form
+function openForm() {
+    //^ closedForm becomes FALSE
+    closedForm = false;
+    //^ Stop the eventListener on contact elements
+    contact.forEach(element => {
+        element.removeEventListener('click', openForm)
+    });
+    //^ Cross is active to close the form
+    cross.addEventListener('click', closeForm);
+
+       
+    //^ Add a class to the form space
+    screen.classList.add('blurryScreen');
+
+    //^ Add the new div and others elements
+    //? New div
+    global.append(divForm);
+    divForm.classList.add('contact-form');
+    divForm.innerHTML = "<h3 class='home__content__title__h3 title-form'>Formulaire de contact</h3>";
+    
+    //? Cross
+    divForm.append(cross);
+    cross.innerHTML = "<i class=\"far fa-times-circle cross-to-quit\"></i>";
+    cross.addEventListener('mouseover', () => {
+        cross.classList.add('on-cross-to-quit');
+    });
+    cross.addEventListener('mouseout', () => {
+        cross.classList.remove('on-cross-to-quit');
+    });
+    cross.addEventListener('click', () => {
+        cross.classList.remove('on-cross-to-quit');
+    });
+    
+};
+
+//^ Creation of function to close the form
+function closeForm() {
+    //^ closedForm becomes TRUE
+    closedForm = true;
+    contact.forEach(element => {
+        element.addEventListener('click', openForm)
+    });
+
+    //^ Replace class of the space form
+    screen.classList.replace('blurryScreen', 'unblurryScreen');
+    //^ Removing element
+    divForm.remove();
+};
