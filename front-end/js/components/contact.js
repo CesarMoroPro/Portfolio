17df6b03b01780ae0f15236c3f0a525b9a85cbbd
CesarMoro <cesar-moro@outlook.fr>
2022-02-03 19:35:56 +0100

    Formulaire de contact : survol de la croix avec changement de couleur et rotation ok, Apparition du formulaire avec une transition ok, mais pas de fermeture de formulaire avec la transition donc à retravailler +  Refonte du formulaire injecté en JS maintenant en html avec un display none + Avancée sur les inputs du formulaire

--- front-end/js/components/contact.js
+++ front-end/js/components/contact.js
@@ -1,8 +1,10 @@
 let global      = document.getElementById('global');
 let contact     = document.querySelectorAll('.contactForm');
 let screen      = document.getElementById('screen');
-let divForm     = document.createElement('div');
-let cross       = document.createElement('div');
+let form        = document.querySelector('.contact-form');
+let cross       = document.querySelector('.cross-to-quit');
+
+
 let closedForm  = true;
 
 
@@ -14,50 +16,36 @@ contact.forEach(element => {
 
 //^ Creation of function to open the form
 function openForm() {
-    //^ closedForm becomes FALSE
+
+    //? 1 - closedForm becomes FALSE
     closedForm = false;
-    //^ Stop the eventListener on contact elements
+    //? 2 - Stop the eventListener on contact elements
     contact.forEach(element => {
         element.removeEventListener('click', openForm)
     });
-    //^ Cross is active to close the form
+    //? 3 - Add a class to the backgound form space
+    screen.classList.replace('unblurryScreen', 'blurryScreen');
+
+    //? 4 -  Changing the form section class
+    form.classList.replace('contact-form', 'contact-form--active');
+
+    //? 5 - Active the cross to close the form
     cross.addEventListener('click', closeForm);
-
-       
-    //^ Add a class to the form space
-    screen.classList.add('blurryScreen');
-
-    //^ Add the new div and others elements
-    //? New div
-    global.append(divForm);
-    divForm.classList.add('contact-form');
-    divForm.innerHTML = "<h3 class='home__content__title__h3 title-form'>Formulaire de contact</h3>";
-    
-    //? Cross
-    divForm.append(cross);
-    cross.innerHTML = "<i class=\"far fa-times-circle cross-to-quit\"></i>";
-    cross.addEventListener('mouseover', () => {
-        cross.classList.add('on-cross-to-quit');
-    });
-    cross.addEventListener('mouseout', () => {
-        cross.classList.remove('on-cross-to-quit');
-    });
-    cross.addEventListener('click', () => {
-        cross.classList.remove('on-cross-to-quit');
-    });
-    
 };
+
 
 //^ Creation of function to close the form
 function closeForm() {
-    //^ closedForm becomes TRUE
+    
+    //? 1 - closedForm becomes TRUE
     closedForm = true;
+    //? 2 - Active the eventListener on contact elements
     contact.forEach(element => {
         element.addEventListener('click', openForm)
     });
-
-    //^ Replace class of the space form
+    //? 3 - Remove the class to the background form space
     screen.classList.replace('blurryScreen', 'unblurryScreen');
-    //^ Removing element
-    divForm.remove();
+
+    //? 4 - Changing the form section class
+    form.classList.replace('contact-form--active', 'contact-form');
 };
