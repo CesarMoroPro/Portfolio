e158920abf3dffb6d53e62a44e9c2f8a24d7cf5e
CesarMoro <cesar-moro@outlook.fr>
2022-03-01 15:11:23 +0100

    Darkmode fonctionnel, avec utilisation d'un booléen pour la condition au lieu du .textContent

--- front-end/js/utils/darkmode.js
+++ front-end/js/utils/darkmode.js
@@ -6,26 +6,29 @@ let darkmodeSelector    = document.getElementById('darkmodeSelector');
 let darkmodeP           = document.getElementById('darkmodeSelector__p');
 let darkmodeIcon        = document.getElementById('icon--darkmode');
 
-if(darkmodeP.textContent === 'Basculer en mode sombre') {
+let activatedDarkmode   = false;
 
-    darkmodeSelector.addEventListener('click', () => {
+
+
+darkmodeP.addEventListener('click', () => {
+    if (activatedDarkmode === false) {
+        activatedDarkmode = true;
+
         home.classList.replace('home--darkmode--off', 'home--darkmode--on');
 
         darkmodeP.textContent = 'Basculer en mode clair';
         darkmodeP.classList.replace('p--darkmode--off', 'p--darkmode--on');
         social.forEach(element =>
             element.classList.replace('social--darkmode--off', 'social--darkmode--on'));
-    });
-} 
-else if(darkmodeP.textContent === 'Bacsuler en mode clair') {
-console.log('%c' + "ici", 'color: red; font-size: 1.5rem');
-    
-    darkmodeSelector.addEventListener('click', () => {
+    }
+    else {
+        activatedDarkmode = false;
+
         home.classList.replace('home--darkmode--on', 'home--darkmode--off');
 
         darkmodeP.textContent = 'Basculer en mode sombre';
         darkmodeP.classList.replace('p--darkmode--on', 'p--darkmode--off');
         social.forEach(element =>
             element.classList.replace('social--darkmode--on', 'social--darkmode--off'));
-    });
-}
+    };
+});
