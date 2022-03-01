95f0492ca357bbbd4bc64edfe7a8d893742536dd
CesarMoro <cesar-moro@outlook.fr>
2022-03-01 09:52:36 +0100

    Changement du darkmode.js : suppression du survol en js pour le passer en :hover en scss

--- front-end/js/utils/darkmode.js
+++ front-end/js/utils/darkmode.js
@@ -6,39 +6,24 @@ let darkmodeSelector    = document.getElementById('darkmodeSelector');
 let darkmodeP           = document.getElementById('darkmodeSelector__p');
 let darkmodeIcon        = document.getElementById('icon--darkmode');
 
-if(darkmodeP.textContent === 'Passer en mode sombre') {
-
-    darkmodeSelector.addEventListener('mouseover', () => {
-        darkmodeP.classList.replace('p--darkmode--off', 'p--darkmode--on');
-    });
-
-    darkmodeSelector.addEventListener('mouseout', () => {
-        darkmodeP.classList.replace('p--darkmode--on', 'p--darkmode--off');
-    });
+if(darkmodeP.textContent === 'Basculer en mode sombre') {
 
     darkmodeSelector.addEventListener('click', () => {
         home.classList.replace('home--darkmode--off', 'home--darkmode--on');
 
-        darkmodeP.textContent = 'Passer en mode clair';
+        darkmodeP.textContent = 'Basculer en mode clair';
         darkmodeP.classList.replace('p--darkmode--off', 'p--darkmode--on');
         social.forEach(element =>
             element.classList.replace('social--darkmode--off', 'social--darkmode--on'));
     });
 } 
-else if(darkmodeP.textContent === 'Passer en mode clair') {
-
-    darkmodeSelector.addEventListener('mouseover', () => {
-        darkmodeP.classList.replace('p--darkmode--on', 'p--darkmode--off');
-     });
-
-    darkmodeSelector.addEventListener('mouseout', () => {
-        darkmodeP.classList.replace('p--darkmode--off', 'p--darkmode--on');
-    });
-
+else if(darkmodeP.textContent === 'Bacsuler en mode clair') {
+console.log('%c' + "ici", 'color: red; font-size: 1.5rem');
+    
     darkmodeSelector.addEventListener('click', () => {
         home.classList.replace('home--darkmode--on', 'home--darkmode--off');
 
-        darkmodeP.textContent = 'Passer en mode clair';
+        darkmodeP.textContent = 'Basculer en mode sombre';
         darkmodeP.classList.replace('p--darkmode--on', 'p--darkmode--off');
         social.forEach(element =>
             element.classList.replace('social--darkmode--on', 'social--darkmode--off'));
